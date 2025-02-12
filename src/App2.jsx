8f26289de78379ca90111644ac03d49a3d04d327
FengCastelo <ianfcastelo7@gmail.com>
2025-02-12 19:37:40 -0300

    Update css + jsx on react

---
+++ src/App2.jsx
@@ -0,0 +1,25 @@
+//rafce (React arrow function component explorer)
+import React from 'react'
+
+const App2 = () => {
+  return (
+    <div>App2</div>
+  )
+}
+
+export default App2
+
+//Name of components
+//PascalCase
+
+//Nomeação de Váriavel/Função: 
+// camelCase
+
+//export default posso importar com qlqr nome e sem chaves!
+//export "no default" só posso importar entre chaves e com o nome exportado
+
+//self closing tag 
+// <Header/> :
+// <Header></Header>
+
+//alt="" > util para qnd surgir alterações/erros ser informado pela tag.

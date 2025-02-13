e84fa23f3973e35fea10af27d616fc96fbaef956
FengCastelo <ianfcastelo7@gmail.com>
2025-02-12 21:36:04 -0300

    Last commit 1fst day

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,14 +12,17 @@ const Main = () => {
                 Mostrar mais
                 </a>
             </div>
-
              <div className='single-item'>
-                <img className='single-item__image' 
-                src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4" 
-                alt="Imagem do Artista X" 
-                />
-
-                <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} />
+             <div className='single-item__div-image-button'>
+                <div className='single-item__div-image'>
+                 <img 
+                 className='single-item__image' 
+                 src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4" 
+                 alt="Imagem do Artista X" 
+                 />
+                </div>
+                 <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} />
+                </div>
 
                 <p>Henrique e Juliano</p>
                 <p>Artista</p>

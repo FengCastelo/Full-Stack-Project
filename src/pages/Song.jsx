9d14c35acc6a671d7ddaa9bdb79298ffa5ebcbba
FengCastelo <ianfcastelo7@gmail.com>
2025-02-16 17:03:11 -0300

    Finish application of player/backward/foward control

--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
+import Player from '../components/Player'
+import { Link } from 'react-router-dom'
 
 const Song = () => {
   return (
     <div className='song'>
       <div className="song__container">
-        <div>
-        <img src="" alt="" />
+        <div className='song__image-container'>
+          <img src="https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24" 
+          alt="Imagem da música X" 
+        />
         </div>
       </div>
 
-      <div className="song__bar"></div>
+      <div className="song__bar">
+         <Link to="/artist/1"className='song__artist-image'>
+            <img 
+              width={70}
+              height={70}
+              src="https://i.scdn.co/image/ab6761610000517499c26e8fb91634b00764bbb9" 
+              alt="Imagem do Artista Y" 
+            />
+         </Link>
+         
+         <Player />
+          
+         <div>
+          <p className='song__name'>Última Saudade - Ao Vivo</p>
+          <p>Henrique & Juliano</p>
+         </div>
+      </div>
     </div>
   )
 }

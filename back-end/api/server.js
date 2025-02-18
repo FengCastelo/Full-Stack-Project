931c64e2e18d1879dcd4e7c9215c4d351d72524f
FengCastelo <ianfcastelo7@gmail.com>
2025-02-17 23:01:05 -0300

    setting endpoints

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,7 +1,10 @@
 //API - Application Programming Interface
 //GET, POST, PATCH, DELETE
 //CRUD Create, Read, Update, Delete
+
 import express from "express";
+import {songsArray} from "../../front-end/src/assets/database/songs.js";
+import {artistArray} from "../../front-end/src/assets/database/artists.js";
 
 
 const app = express();
@@ -11,13 +14,13 @@ app.get("/", (request, response) => {
     response.send('Now we will use just the /artists and /songs endpoints ')
 })
 app.get("/artists", (request, response) => {
-    response.send()
+    response.send(artistArray)
 })
 app.get("/songs", (request, response) => {
-    response.send('')
+    response.send(artistArray)
 })
 app.get("/", (request, response) => {
-    response.send('')
+    response.send(songsArray)
 })
 
 app.listen(PORT, () => {

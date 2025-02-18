503cda8430f3bc51c5ad02bbdac9edc15b2b52e9
FengCastelo <ianfcastelo7@gmail.com>
2025-02-17 23:57:33 -0300

    setting connection w/ db

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -3,11 +3,11 @@
 
 import {MongoClient} from "mongodb";
 
-const URI = "mongodb+srv://Fengcastelo:<db_password>@cluster0.opzgr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const URI = "mongodb+srv://Fengcastelo:0311castelo@cluster0.opzgr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(URI);
 
-const database = client.db('spotifyproj');
+const db = client.db("spotifyproj");
 const songCollection = await db.collection("songs").find({}).toArray();
 
-console.log(songCollection)
+console.log(songCollection);
